f44e2e096ed15a0ad1dabfa7e392c575996ae58d
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-06-17 23:06:37 +0100

    Implement GET endpoint for retrieving a tour by ID with error handling

--- app.js
+++ app.js
@@ -30,6 +30,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  
+  // if(id > tours.length) {
+  if(!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+     });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -51,9 +72,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log('App running on port 3000...');
 });
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 28260c8 (Add tour page for The Park Camper Tour with detailed itinerary and reviews)
