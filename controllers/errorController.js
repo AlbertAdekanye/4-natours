bcf2088dcc7a606f7db81d3fe88c6758e4ac8815
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-07-04 00:02:43 +0100

    fix: remove duplicate production error handling condition

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,44 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational error, trusted error: send message to client
+  if(err.isOperational) {
+    res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+
+  // programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    })
+  }
+};
+
 module.exports = (err, req, res, next) => {
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-}
+  if(process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
