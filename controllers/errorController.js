b26c3a1247b15ef6042ae0bab8c98ddaab18635c
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-07-04 15:15:22 +0100

    fix: correct error handling logic for production environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`; 
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsErrorDB = err => {
+  const value = Object.values(err.keyValue)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}; 
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
     status: err.status,
@@ -37,8 +57,17 @@ module.exports = (err, req, res, next) => {
   if(process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+    error.code = err.code;
+    error.keyValue = err.keyValue;
+    error.errors = err.errors;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsErrorDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
