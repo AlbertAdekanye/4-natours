ba5f48949ef1c07c18ceb5920266ca9087582380
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-07-28 02:03:16 +0100

    chore: clean up unused imports in authController and userModel

--- controllers/authController.js
+++ controllers/authController.js
@@ -161,18 +161,27 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     .digest('hex');
 
   const user = await User.findOne({
-    passwordRestToken: hashedToken, 
-    passwordRestExpires: { $gt: Date.now() }
+    passwordResetToken: hashedToken, 
+    passwordResetExpires: { $gt: Date.now() }
   }); 
 
   // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400))
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
 
   // 3) Update changedPasswordAt property for user
 
   // 4) Log the user in, send JWT
+  const token = signToken(user._id);
 
+  res.status(200).json({
+    status: 'success',
+    token
+  });
 });
-
-exports.password = catchAsync(async (req, res, next) => {
-  
-})
