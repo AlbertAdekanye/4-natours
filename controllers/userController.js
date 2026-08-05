95888b5194bb6c9af5d64b2d6cfd3799c5874150
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-05 22:04:55 +0100

    feat: add getOne and getAll methods to handlerFactory and update controllers to use them

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,19 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 }
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-    const users = await User.find();
-
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-        users
-      }
-    });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) create an error if user post password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -60,19 +47,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not defined! and Please use /signup instead'
   });
 };
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+
 // do not update passwords with this!
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
