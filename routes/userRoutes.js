5fdcf01b3cad1208985c55e8aef03e0214e5a0ed
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-05 22:32:40 +0100

    feat: enhance route protection and access control for user, tour, and review routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,21 +17,24 @@ router
 router
  .patch('/resetPassword/:token', authController.resetPassword); 
 
+//  protect all routes after this middleware 
+router.use(authController.protect);
+
 router.patch(
   '/updateMyPassword', 
-  authController.protect, 
   authController.updatePassword
 );
  
 router.get(
   '/me',
-  authController.protect, 
   userController.getMe, 
   userController.getUser
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/deleteMe', authController.protect, userController.deleteMe)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/deleteMe', userController.deleteMe)
+
+router.use(authController.restrictTo('admin'));
 
  router
   .route('/')
