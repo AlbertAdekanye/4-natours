a9ae219131d88da3231bdb4433c75c0d0de84c92
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-06 15:54:41 +0100

    feat: add unique index for user and tour in review schema and round ratings to one decimal place

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   // this.populate({
   //   path: 'tour',
@@ -64,7 +66,7 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
       }
     }
   ]);
-  console.log(stats);
+  // console.log(stats);
 
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
@@ -86,7 +88,7 @@ reviewSchema.post('save', function() {
 
 reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
-  console.log(this.r);
+  // console.log(this.r);
 });
 
 reviewSchema.post(/^findOneAnd/, async function() {
