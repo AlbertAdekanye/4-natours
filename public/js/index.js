1d5cc58862c4b1dc60433ea05b45d4210dd6d754
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-10 15:56:08 +0100

    feat: add user data update functionality
    - Implemented updateData function in updateSettings.js to handle user data updates via API.
    - Integrated updateData function in index.js to submit user data form.
    - Updated account.pug to include user data form for updating name and email.

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill';
 import { displayMap } from './map';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // DOM elements
 const mapEl = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // MAP
 if (mapEl) {
@@ -26,3 +28,11 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout)
+
+if (userDataForm) 
+  userDataForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  })
