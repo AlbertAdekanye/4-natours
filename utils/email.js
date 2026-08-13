646caf33c889b99043dacc0072f50c7747d17415
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2026-08-13 19:53:50 +0100

    feat: implement email templates and welcome email functionality

--- utils/email.js
+++ utils/email.js
@@ -1,18 +1,18 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const htmlToText = require('html-to-text');
+const { convert } = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.name.sp;it(' ')[0];
+    this.firstName = user.name.split(' ')[0];
     this.url = url;
     this.from = `Albert Adekanye <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // sendgrid
+      // SendGrid
       return 1;
     }
 
@@ -21,37 +21,37 @@ module.exports = class Email {
       port: process.env.EMAIL_PORT,
       auth: {
         user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-    }
-  });
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   }
 
-  // send the actual email
+  // Send the actual email
   async send(template, subject) {
-    // 1) render HTML based on a pug template
+    // 1) Render HTML based on a Pug template
     const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
+      `${__dirname}/../views/email/${template}.pug`,
       {
         firstName: this.firstName,
         url: this.url,
-        subject
+        subject,
       }
     );
 
-    // 2)Define the email options
+    // 2) Define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject,
       html,
-      text: htmlToText.fromString(html)
-    }
+      text: convert(html),
+    };
 
-  // 3) create a transport and send email
+    // 3) Create transport and send email
     await this.newTransport().sendMail(mailOptions);
-  };
+  }
 
   async sendWelcome() {
-   await this.send('welcome', 'Welcome to the Natours Family!');
+    await this.send('welcome', 'Welcome to the Natours Family!');
   }
 };
